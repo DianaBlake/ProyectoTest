88dc2fac68e297551b98fe0cc25ab4bf029f6d30
DianaBlake <119768729+DianaBlake@users.noreply.github.com>
2023-06-21 00:13:13 -0600

    Add files via upload

---
+++ Proyect-ENUFC-master/Proyect--ENUFC-master/src/index.js
@@ -0,0 +1,14 @@
+
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import React from 'react';
+
+
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+
+    root.render(
+        <div id='app'>
+            <App/>
+        </div>
+    );
